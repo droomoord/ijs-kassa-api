b3b5e0465c80f0da0bebb4f0e78c1b263831087d
Kris <heijnenkris@gmail.com>
2021-06-13 12:31:21 +0200

    add static files

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("static"));
+
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   next();
